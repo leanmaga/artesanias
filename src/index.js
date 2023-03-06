e43c126e0d3d1b7b8ec37bac3aa2b12d06060a3e
Leandro Magallanes <thesharkcommunity@gmail.com>
2023-03-05 23:13:12 -0300

    modificacion de ruta

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './App.jsx';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
 
